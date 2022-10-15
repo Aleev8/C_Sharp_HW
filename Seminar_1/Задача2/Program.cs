b5538cdbd4510a24027382560be842baa3ef2aeb
zirtuff <aleev.alexandr@gmail.com>
2022-10-15 22:30:49 +0600

    Решена задача 2

--- Seminar_1/Задача2/Program.cs
+++ Seminar_1/Задача2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 2: Напишите программу, которая на вход принимает 
+два числа и выдаёт, какое число большее, а какое меньшее */
+
+Console.Write("Введите чило №1:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите чило №2:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+Console.WriteLine($"Первое число {numberA} большее ");
+}
+if (numberA < numberB)
+{
+    Console.WriteLine($"Второе число {numberB} большее ");
+}
