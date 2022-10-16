eb1ff50b3843bb5fa96d737196ac75f3a3c159f7
zirtuff <aleev.alexandr@gmail.com>
2022-10-16 18:08:51 +0600

    Исправление ошибки задача6

--- Seminar_1/Задача8/Program.cs
+++ Seminar_1/Задача8/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N),
+ а на выходе показывает все чётные числа от 1 до N.*/
+Console.Write("Введите чиcло:...");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while(count <= number)
+{
+    if (count % 2 == 0);
+    Console.WriteLine(count);
+    count++;
+}
+
