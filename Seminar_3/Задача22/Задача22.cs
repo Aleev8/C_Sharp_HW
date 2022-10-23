8a5d132363187803940ab776e64b3ffdbbe6d8aa
zirtuff <aleev.alexandr@gmail.com>
2022-10-23 20:41:32 +0600

    Изменена Задача22

--- Seminar_3/Задача22/Задача22.cs
+++ Seminar_3/Задача22/Задача22.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 23: Напишите программу, которая принимает на вход число (N)
+и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 4, 9. 
+5 -> 1, 8, 27, 64, 125 */
+
+void PrintSquaresToConsole(int n)
+{
+    /* Цикл for (<начальное значение счетчика>; <конечное здначение>; 
+    <что происходит со счетчиком на каждом шаге>)*/
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine(i * i);
+    }
+}
+
+PrintSquaresToConsole(10);
