bfc0d6fc9de328ed3cd42d75a4e7d624f004ac87
Zirtuff_PC <aleev.alexandr@gmail.com>
2022-11-11 22:50:15 +0600

    Решена Задача25

--- Seminar_4/Задача25/Задача 25.cs
+++ Seminar_4/Задача25/Задача 25.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 25: Напишите программу, которая принимает на вход два 
+числа (A и B) и возводит число A в натуральную степень B. 
+Для решения задания использование цикла for является 
+обязательным условием. Не использовать встроенный метод 
+возведения в степень.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+int GetPow(int numA, int numB)
+{
+    int pow = numA;
+    for (int i = 1; i < numB; i++)
+    {
+        pow = pow * numA;
+    }
+    
+    return pow;
+}
+
+Console.Write("Введите число А: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int pow = GetPow(numA, numB);
+Console.WriteLine(pow);
