89b709eec12c104e9643390be545ff117f761b6f
Zirtuff_PC <aleev.alexandr@gmail.com>
2022-11-12 14:08:30 +0600

    Решена Задача29

--- Seminar_4/Задача29/Задача29.cs
+++ Seminar_4/Задача29/Задача29.cs
@@ -3,3 +3,13 @@
 1, 2, 5, 7, 19-> [1, 2, 5, 7, 19]
 6, 1, 33-> [6, 1, 33] */
 
+Console.Write("Введите желаемое количество элементов создаваемого массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите элемент массива с индексом {i}:");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("[{0}]", string.Join(", ", array));
