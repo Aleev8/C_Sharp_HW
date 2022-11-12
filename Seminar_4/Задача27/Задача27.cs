e12f32ccb3dca795d0e636aa680a55f584d1c47e
Zirtuff_PC <aleev.alexandr@gmail.com>
2022-11-12 13:46:33 +0600

    Решена Задача27

--- Seminar_4/Задача27/Задача27.cs
+++ Seminar_4/Задача27/Задача27.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 27: Напишите программу, которая принимает на вход число
+ и выдаёт сумму цифр в числе. Для решения задания использование 
+ цикла for является обязательным условием
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+
+int SumOfDigits(int number)
+{
+    int sum = 0;
+    for (; number > 0 ;)
+    {
+        sum = sum + number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumOfDigits(number));
+
+
