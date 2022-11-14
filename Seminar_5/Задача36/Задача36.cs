e581ad7a7a0b0a4957980df9f331cd519bbc649a
zirtuff <aleev.alexandr@gmail.com>
2022-11-14 19:21:00 +0600

    Решена Задача36

--- Seminar_5/Задача36/Задача36.cs
+++ Seminar_5/Задача36/Задача36.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int SumNotEvenElement(int[] number)
+{
+    int sum = number[0];
+    for (int i = 1; i < number.Length; i++)
+    {
+
+        if (i % 2 != 0)
+        {
+            sum += number[i];
+        }
+    }
+    return sum;
+}
+
+int[] array = new int[16];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+}
+Console.WriteLine("[{0}]", string.Join(", ", array));
+Console.WriteLine($"Сумма элементов стоящих на нечетных позициях = {SumNotEvenElement(array)}");
