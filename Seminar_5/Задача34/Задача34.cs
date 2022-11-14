f24e78c84136f1bbd7cd820d05696b24f9670376
zirtuff <aleev.alexandr@gmail.com>
2022-11-14 19:18:47 +0600

    Решена Задача34

--- Seminar_5/Задача34/Задача34.cs
+++ Seminar_5/Задача34/Задача34.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+ * Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+*/
+
+int VolumeEvenNum(int[] numbers)
+{
+    int count = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        
+        if (numbers[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+
+int[] array = new int[16];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+}
+Console.WriteLine("[{0}]", string.Join(", ", array));
+Console.WriteLine($"Количество четных чисел в массиве = {VolumeEvenNum(array)}");
+
