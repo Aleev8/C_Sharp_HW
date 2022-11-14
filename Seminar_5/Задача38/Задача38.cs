eeec71db5ce7e199b32b14f354bbb44220949adf
zirtuff <aleev.alexandr@gmail.com>
2022-11-14 19:22:46 +0600

    Решена Задача38

--- Seminar_5/Задача38/Задача38.cs
+++ Seminar_5/Задача38/Задача38.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 38: Задайте массив вещественных чисел. 
+ Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+
+double DifferenceMinMax(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (max < array[i])
+        {
+            max = array[i];
+        }
+        if (min > array[i])
+        {
+            min = array[i];
+        }
+    }
+    double result = max - min;
+    return result;
+}
+
+double[] array = new double[8];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().NextDouble();
+}
+Console.WriteLine("[{0}]", string.Join("; ", array));
+Console.WriteLine($"Разница между MAX и MIN элементом массива = {DifferenceMinMax(array)}");
+
