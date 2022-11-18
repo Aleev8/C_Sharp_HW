143c80fb81bb87ceb7e2f3e83b3cf1d1249721b7
zirtuff <aleev.alexandr@gmail.com>
2022-11-18 14:06:02 +0600

    решена Задача41

--- Seminar_6/Задача41/Задача41.cs
+++ Seminar_6/Задача41/Задача41.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+
+Console.Write("Введите числа(разделяя пробелом): ");
+int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Кол-во элементов > 0: {count}");
