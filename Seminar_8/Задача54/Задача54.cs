5f843dfe4b9dbb506074aa38d39a47773f38a79a
Zirtuff_PC <aleev.alexandr@gmail.com>
2022-11-24 18:41:46 +0600

    Решена Задача54

--- Seminar_8/Задача54/Задача54.cs
+++ Seminar_8/Задача54/Задача54.cs
@@ -9,3 +9,54 @@
 9 5 3 2
 8 4 4 2
 */
+
+void FillArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 11);
+        }
+    }
+}
+
+void PrintArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] +" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] DescendingSort (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - j - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = temp;
+                }
+            }
+        }
+    }
+    
+    return array;
+}
+
+int[,] testArray = new int[new Random().Next(3, 8), new Random().Next(3, 7)];
+
+FillArray(testArray);
+PrintArray(testArray);
+Console.WriteLine();
+PrintArray(DescendingSort(testArray));
