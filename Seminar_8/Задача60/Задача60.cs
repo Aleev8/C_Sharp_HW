9f80afb4dc71a5026433720a068e0fcceb3da426
Zirtuff_PC <aleev.alexandr@gmail.com>
2022-11-25 17:49:58 +0600

    Решена Задача60

--- Seminar_8/Задача60/Задача60.cs
+++ Seminar_8/Задача60/Задача60.cs
@@ -5,3 +5,65 @@
 34(1,0,0) 41(1,1,0)
 27(0,0,1) 90(0,1,1)
 26(1,0,1) 55(1,1,1) */
+
+
+
+void PrintArray (int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("| ");
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]} ({i}, {j}, {k}) | ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+int[,,] GetArray(int size)
+{
+    int[,,] result = new int[size, size, size];
+
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            int k = 0;
+            while (k < result.GetLength(2))
+            {
+                int element = new Random().Next(10, 99 + 1);
+                if (FindElement(result, element)) continue;
+                result[i, j, k] = element;
+                k++;
+            }
+        }
+    }
+    return result;
+}
+
+
+        bool FindElement(int[,,] array, int el)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                if (array[i, j, k] == el) return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+Console.WriteLine("Введите размерность трехмерного массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+PrintArray(GetArray(size));
