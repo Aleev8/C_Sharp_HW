f83ca165e6b818370b4ec829d70b179c0efe911d
Zirtuff_PC <aleev.alexandr@gmail.com>
2022-11-25 18:54:37 +0600

    Решена Задача62

--- Seminar_8/Задача62/Задача62.cs
+++ Seminar_8/Задача62/Задача62.cs
@@ -4,3 +4,45 @@
 12 13 14 05
 11 16 15 06
 10 09 08 07 */
+
+
+void FillArraySpirally (int[,] array)
+{
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= array.GetLength(0) * array.GetLength(1))
+{
+  array[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < array.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= array.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > array.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+}
+
+void PrintArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] +" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int size = new Random().Next(3, 8);
+
+int[,] testArray = new int[size, size];
+
+
+FillArraySpirally(testArray);
+PrintArray(testArray);
